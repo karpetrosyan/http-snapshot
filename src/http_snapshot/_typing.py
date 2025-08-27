9c6ba9995669d869fba393f876d338bc13493fd6
Kar Petrosyan <kar.petrosyanpy@gmail.com>
2025-08-28 02:46:33 +0400

    chore: fix linting for newer python versions

--- src/http_snapshot/_typing.py
+++ src/http_snapshot/_typing.py
@@ -3,7 +3,7 @@ from typing import Any
 import sys
 
 if sys.version_info >= (3, 11):
-    from typing import assert_never
+    from typing import assert_never as assert_never
 else:
 
     def assert_never(value: Any, message: str = "Unexpected value") -> Any:
