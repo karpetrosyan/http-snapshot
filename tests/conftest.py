201d4f44a8c7a733d18e4dfd72572060cc03d91e
Kar Petrosyan <kar.petrosyanpy@gmail.com>
2025-12-03 17:29:56 +0400

    fix bug that adds the http-record flag two times

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,12 @@
+from http_snapshot._utils import suppress_errors
 import pytest
 
 
 from http_snapshot._pytest_plugin import *  # noqa: F403
+from http_snapshot._pytest_plugin import pytest_addoption
+
+# TODO: understand why pytest_addoption is called multiple times by pytest
+pytest_addoption = suppress_errors(RuntimeError)(pytest_addoption)
 
 
 @pytest.fixture
