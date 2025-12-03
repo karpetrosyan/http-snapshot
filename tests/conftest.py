723d65533c639bd1794362d437158285915590a4
Kar Petrosyan <kar.petrosyanpy@gmail.com>
2025-12-03 17:33:11 +0400

    fallback to False if option wasn't found

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,7 @@
-from http_snapshot._utils import suppress_errors
 import pytest
 
 
-from http_snapshot._pytest_plugin import *  # noqa: F403
-from http_snapshot._pytest_plugin import pytest_addoption
-
-# TODO: understand why pytest_addoption is called multiple times by pytest
-pytest_addoption = suppress_errors(RuntimeError)(pytest_addoption)
+from http_snapshot._fixtures import *  # noqa: F403
 
 
 @pytest.fixture
